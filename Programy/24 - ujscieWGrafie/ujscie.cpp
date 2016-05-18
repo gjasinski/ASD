dddd898f4c315d1b2dfb724432d3af3ff05b3d8b
gjasinski <gjasinski@hotmail.com>
2016-05-18 17:26:30 +0200

    ujscie w grafie rep. macierzowa - liniowo

--- Programy/24 - ujscieWGrafie/ujscie.cpp
+++ Programy/24 - ujscieWGrafie/ujscie.cpp
@@ -10,3 +10,52 @@ Sa dwie petle, ale za kazdym razem gdy jest jakas krawedz eliminujemy jedna z mo
 
 */
 
+#include <stdlib.h>
+#include <stdio.h>
+
+bool ujscie(int ** G, int n);
+
+int main(void){
+    int n=5;
+    int** G= new int*[n];
+    for(int i=0;i<n;i++) G[i]=new int[n];
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)G[i][j]=0;
+    G[0][3]=G[1][3]=G[2][3]=G[4][3]=1;
+    if(ujscie(G,n)) printf("Jest ujscie");
+        else printf("Nie ma ujscia");
+}
+void print(int** G, int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++) printf("%d ", G[i][j]);
+        printf("\n");
+    }
+}
+
+bool ujscie(int ** G, int n){
+    int* wynik = new int[n];
+    for(int i=0;i<n;i++) wynik[i]=1;
+    print(G, n);
+    for(int i=0;i<n;i++){
+        if(wynik[i]==0) continue;
+        for(int j=0;j<n;j++){
+            printf("%d %d\n", i,j);
+            if(i==j)continue;
+            if(G[i][j]==1){
+                wynik[i]=0;
+            }else{
+                wynik[j]=0;
+            }
+            if(G[j][i]==1){
+                wynik[j]=0;
+            }
+            else{
+                wynik[i]=0;
+            }
+        }
+    }
+    for(int i=0;i<n;i++) printf("%d ", wynik[i]);
+    for(int i=0;i<n;i++) if(wynik[i]) return true;
+    return false;
+}
+
