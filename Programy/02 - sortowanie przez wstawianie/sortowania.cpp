a596a684128f16d08b0ae08d53d23ef227a084c5
gjasinski <gjasinski@hotmail.com>
2016-06-11 18:09:54 +0200

    fix

--- Programy/02 - sortowanie przez wstawianie/sortowania.cpp
+++ Programy/02 - sortowanie przez wstawianie/sortowania.cpp
@@ -1,7 +1,9 @@
+//wszystkie listy sa z straznikiem
+
 #include <iostream>
 #include <limits.h>
 #include <stdlib.h>
-//wsztstkie listy sa z straznikiem
+
 using namespace std;
 
 struct node{
@@ -11,37 +13,31 @@ struct node{
 
 
 void minmax(int A[], int n, int &maks, int &min);
-int main()
-{
+
+int main(){
     const int n=100;
     int A[n];
     srand(time(NULL));
-    for(int i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++){
         A[i]=rand()%1000;
         cout<<A[i]<<" ";
     }
     int min, maks;
     minmax(A,n,maks, min);
     cout<<endl<<min<< "  "<<maks;
-
 }
 
 
-void insert(node *list, node *e)
-{
-    while(list->next!=NULL and list->next->key<e->key)
-    {
+void insert(node* list, node* e){
+    while(list->next!=NULL and list->next->key<e->key){
         list=list->next;
     }
     list->next=e;
 }
 
-node *removeMax(node *list)
-{
+node* removeMax(node* list){
     node *tmp=list;
-    while(list->next!=NULL)
-    {
+    while(list->next!=NULL){
         if(list->next->key>=tmp->next->key)
         //if(list->next->key>tmp->next->key) werjsja niestablina
             tmp=list;
@@ -53,15 +49,13 @@ node *removeMax(node *list)
     return r;
 }
 
-node *inertsort(node *list)
-{
+node* inertsort(node* list){
     node *tmp;
     node *sorted = new node;
     sorted->next=NULL;
-    while(list->next!=NULL)
-    {
+    while(list->next!=NULL){
         tmp=list->next;
-        list->next=tmp->next;
+        list->next=list->next;
         tmp->next=NULL;
         insert(sorted, tmp);
     }
@@ -69,13 +63,11 @@ node *inertsort(node *list)
     return sorted;
 }
 
-node *ssort(node *list)
-{
+node* ssort(node* list){
     node *tmp;
     node *sorted=new node;
     sorted->next=NULL;
-    while(list->next!=NULL)
-    {
+    while(list->next!=NULL){
         tmp=removeMax(list);
         tmp->next=sorted->next;
         sorted->next=tmp;
@@ -90,15 +82,12 @@ void minmax(int A[], int n, int &maks, int &min){
     int i,j;
     i=0;
     j=n-1;
-    do
-    {
-        if(A[i]<A[j])
-        {
+    do{
+        if(A[i]<A[j]){
             if(min>A[i]) min=A[i];
             if(maks<A[j])maks=A[j];
         }
-        else
-        {
+        else{
             if(min>A[j])min=A[j];
             if(maks<A[i])maks=A[i];
         }
